bb5cb967e3377179d55c98a8b4fd98051f9c79c6
louise <dachtedreams@gmail.com@gmail.com>
2020-12-25 00:10:14 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,15 @@
 # 當發生錯誤時終止腳本運行
 set -e
 # 打包
-yarn build
+# yarn build
 # yarn generate
 # // 打包靜態資源的是在 docs 目錄
 # 移動至到打包後的dist目錄 
 cd docs
 git init
 # //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
-git add -A
-git commit -m 'deploy'
+# git add -A
+# git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
 # https://github.com/wan2019/chiwanlive.git
 git push -f https://github.com/wan2019/chiwanlive.git master:gh-pages
