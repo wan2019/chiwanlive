e85b3715949da996c25672c52a4c4feeedd53222
louise <dachtedreams@gmail.com@gmail.com>
2022-01-25 01:58:58 +0800

    add 攝影頁

---
+++ components/studio/M0202.vue
@@ -0,0 +1,86 @@
+<template>
+<section class="box">
+  <div class="box__item">
+    <v-row id="service" class="service">
+      <v-col v-for="(item, i) in list2" cols="12" sm="6" md="4" :key="`list2${i}`"  class="test__red">
+        <ImgBox :item="item"></ImgBox>
+      </v-col>
+    </v-row>
+  </div>
+</section>
+</template>
+
+<script>
+import ImgBox from '@/components/studio/M0202ImgBox.vue'
+
+export default {
+  name: '',
+  components: {
+    ImgBox,
+  },
+  data() {
+    return {
+      list2: [ // FIXME: 攝影圖片
+        {
+          alt: '活動直播－線下活動　線上直播　觀看人數倍數成長',
+          src: 'img/liveStream_8.jpg',
+          text: ['訪談'],
+          aosDelay: 1,
+        },
+        {
+          alt: '4G包直播 8張網卡聚合  外景直播不斷線',
+          src: 'img/OutdoorLiveBroadcast_12.jpg',
+          text: ['訪談'],
+          aosDelay: 2,
+        },
+        {
+          alt: '視訊直播 遠端視訊　即時對話　全球來賓任你邀',
+          src: 'img/RemoteVideo_10.jpg',
+          text: ['訪談'],
+          aosDelay: 3,
+        },
+        {
+          alt: '現場轉播 多機畫面 大螢幕現場LIVE',
+          src: 'img/LiveBroadcast_11.jpg',
+          text: ['訪談'],
+          aosDelay: 4,
+        },
+        {
+          alt: '現場轉播 多機畫面 大螢幕現場LIVE',
+          src: 'img/LiveBroadcast_11.jpg',
+          text: ['訪談'],
+          aosDelay: 5,
+        },
+        {
+          alt: '現場轉播 多機畫面 大螢幕現場LIVE',
+          src: 'img/LiveBroadcast_11.jpg',
+          text: ['訪談'],
+          aosDelay: 6,
+        }
+      ]
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.imgLogo {
+  height: 1em;
+  vertical-align:middle;
+}
+.text {
+  &__center {
+    text-align: center;
+  }
+}
+.test__red {
+  display: flex;
+}
+.box {
+  position: relative;
+  padding: 3rem;
+}
+.service {
+  margin-bottom: 2rem;
+}
+</style>
---
+++ components/studio/M0203ImgBox.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="imgBox">
+    <div data-aos="fade-right" :data-aos-delay="aosDelay">
+      <div class="imgBox__container">
+        <div :style="`width:${item.width}px`">
+          <v-img
+            :src="item.src"
+            :alt="item.alt"
+            :aspect-ratio="aspectRatio"
+            class="imgBox__img"
+            :style="`margin-bottom:${item.bottom}px`"
+            eager
+            contain
+          ></v-img>
+        </div>
+      </div>
+      <div class="imgBox__textBox">
+        <template v-for="(each ,k) in item.text" >
+          <h6 v-if="k === 0" :key="k" class="imgBox__text imgBox__title">
+            {{each}}
+          </h6>
+          <h6 v-else :key="k" class="imgBox__text">{{each}}</h6>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ImgBox',
+  props: {
+    item: [Object],
+    aspectRatio: {
+      type: [Number, String],
+      default: 6/4,
+    }
+  },
+  computed: {
+    aosDelay() {
+      return this.item.aosDelay*300
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.imgBox {
+  width: 100%;
+  &__container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 250px;
+  }
+  &__textBox {
+    margin: auto;
+  }
+  &__text {
+    z-index: 100;
+    margin-top: 0.25rem;
+    font-size: 1.5rem;
+  }
+  &__textFirst {
+    font-size: 1.5rem;
+  }
+}
+h6 {
+  font-size: 16px;
+}
+.imgBox {
+  &__text {
+    text-align: center;
+    z-index: 100;
+    margin-top: 0.25rem;
+    font-size: 1.25rem;
+  }
+  &__title {
+    font-size: 1.5rem;
+    padding-bottom: 1rem;
+  }
+}
+</style>
